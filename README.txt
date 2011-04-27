52e7c3f9fc8c0ddcb6330d777a92c9d3ba07401c
Josef Richter <josef.richter@me.com>
2011-04-27 20:26:43 +0200

    basic plugin working

---
+++ README.txt
@@ -0,0 +1,9 @@
+jQuery grayscale
+
+converts image(s) to grayscale using html5 canvas
+
+requires jQuery, HTML5 canvas
+
+usage:                   
+
+		$('img').grayscale();
---
+++ js/grayscale.js
@@ -0,0 +1,52 @@
+// jQuery grayscale plugin
+// converts color images to grayscale using canvas element
+// requires jQuery and canvas compatible browser
+// (c) Josef Richter 2011
+// github.com/josefrichter/jquery_grayscale
+
+(function( $ ){
+
+  $.fn.grayscale = function() {
+  
+		return this.each(function(){    
+
+			var $this = $(this);
+				
+			$this.load(function(){
+					
+				var canvas = document.createElement('canvas');
+			  var ctx = canvas.getContext('2d');
+
+			  var imgObj = new Image();
+			  imgObj.src = $this.attr('src');
+
+			  canvas.width = imgObj.width;
+			  canvas.height = imgObj.height;
+
+			  ctx.drawImage(this, 0, 0);
+
+
+			  var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+			  for(var y = 0; y < imgPixels.height; y++){
+		    	for(var x = 0; x < imgPixels.width; x++){
+		      	var i = (y * 4) * imgPixels.width + x * 4;
+		        var avg = (imgPixels.data[i] + 
+		                   imgPixels.data[i + 1] + 
+		                   imgPixels.data[i + 2]
+		                    ) / 3;
+		        imgPixels.data[i] = avg; 
+		        imgPixels.data[i + 1] = avg; 
+		        imgPixels.data[i + 2] = avg;
+		      }
+		    }
+
+		    ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+				$this.attr('src',canvas.toDataURL());
+				
+				});
+
+		});
+
+  };
+})( jQuery );
