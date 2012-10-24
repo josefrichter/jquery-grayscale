fe70edf1058a951ea83ca4eed4867df02f3e6ad6
Anthony McLin <mailfrom@anthonymclin.com>
2012-10-24 09:49:17 -0700

    Grayscale calculation using standard Luma formula
    The standard "Luma" formula is closer to how humans perceive colors and grayscale. While an average is technically correct, it doesn't match what the human eye perceives.
    
    See http://www.tannerhelland.com/3643/grayscale-image-algorithm-vb6/ for more explanation.

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,11 +39,13 @@ return this.each(function(){
 		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	
 		for(var i = 0, n = imgPixels.data.length; i < n; i += 4) {
-			var avg = (imgPixels.data[i + 0] +  imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-					
-			imgPixels.data[i + 0] = avg; 
-			imgPixels.data[i + 1] = avg; 
-			imgPixels.data[i + 2] = avg;
+			//Calculate monochrome value using Classic Luma formula used by most image processing software
+			var avg = imgPixels.data[i + 0] * 0.2126 + imgPixels.data[i + 1] * 0.7152 + imgPixels.data[i + 2] * 0.0722; 
+
+			imgPixels.data[i + 0] = avg; //red channel
+			imgPixels.data[i + 1] = avg; //blue channel
+			imgPixels.data[i + 2] = avg; //green channel
+			// i + 4 would be the alpha channel
 		}
 	
 				ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
