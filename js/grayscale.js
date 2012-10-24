46bd704a3f4abf9a05a627c0135c6b868576721d
Anthony McLin <mailfrom@anthonymclin.com>
2012-10-24 09:35:44 -0700

    Reduce loops and pixel position math
    Since the pixel rewrite loop will run literally thousands of times on just a single image, it makes sense to optimize it as much as possible.
    
    Reducing to a single iterative loop instead of two nested loops.
    
    Also reduce the math run inside of each loop to calculate the pixel position in the array.

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,16 +38,13 @@ return this.each(function(){
 	
 		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	
-			for(var y = 0; y < imgPixels.height; y++){
-				for(var x = 0; x < imgPixels.width; x++){
-					var i = (y * 4) * imgPixels.width + x * 4;
-					var avg = (imgPixels.data[i + 0] +  imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+		for(var i = 0, n = imgPixels.data.length; i < n; i += 4) {
+			var avg = (imgPixels.data[i + 0] +  imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
 					
-					imgPixels.data[i + 0] = avg; 
-					imgPixels.data[i + 1] = avg; 
-					imgPixels.data[i + 2] = avg;
-				}
-			}
+			imgPixels.data[i + 0] = avg; 
+			imgPixels.data[i + 1] = avg; 
+			imgPixels.data[i + 2] = avg;
+		}
 	
 				ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
 					$this.attr('src',canvas.toDataURL());
